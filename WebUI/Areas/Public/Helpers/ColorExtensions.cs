6e0151af104f9c3deb79ac40b2ffd857f322093a
unknown <smirnovvv@dev-dep-12.npc.ba>
2016-10-11 21:36:14 +0300

    last update

---
+++ WebUI/Areas/Public/Helpers/ColorExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace WebUI.Areas.Public.Helpers
+{
+    public class ColorExtensions
+    {
+        public static Color GetColor(string color)
+        {
+            Color c;
+
+            if (color.IndexOf("RGB", StringComparison.InvariantCultureIgnoreCase) != -1)
+            {
+                var colors = Regex.Matches(color, @"[+-]?\d+(\.\d+)?").Cast<Match>()
+                    .Select(x => int.Parse(x.Value)).ToArray();
+
+                c = Color.FromArgb(colors[3], colors[0], colors[1], colors[2]);
+            }
+            else
+            {
+                c = ColorTranslator.FromHtml(color);
+            }
+
+            return c;
+        }
+
+        public static Color ChangeColorBrightness(Color color, float correctionFactor)
+        {
+            var red = (float)color.R;
+            var green = (float)color.G;
+            var blue = (float)color.B;
+
+            if (correctionFactor < 0)
+            {
+                correctionFactor = 1 + correctionFactor;
+                red *= correctionFactor;
+                green *= correctionFactor;
+                blue *= correctionFactor;
+            }
+            else
+            {
+                red = (255 - red) * correctionFactor + red;
+                green = (255 - green) * correctionFactor + green;
+                blue = (255 - blue) * correctionFactor + blue;
+            }
+
+            return Color.FromArgb(color.A, (int)red, (int)green, (int)blue);
+        }
+    }
+
+
+}
